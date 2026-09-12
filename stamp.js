6dffd6efb6ee01dbf22b339d758010e8b3381c42
CJD0314 <danielsc0314@gmail.com>
2026-09-11 22:03:09 -0400

    Inject site nav on every board

--- stamp.js
+++ stamp.js
@@ -1,8 +1,19 @@
+function splitNav(){
+  var html = '<a href="today.html">TODAY</a><a href="index.html">HOME</a><a href="reviews.html">BETTING REVIEWS</a>';
+  var nav = document.querySelector(".topnav");
+  if (!nav) {
+    nav = document.createElement("div");
+    nav.className = "topnav";
+    document.body.insertBefore(nav, document.body.firstChild);
+  }
+  if (nav.innerHTML.indexOf("reviews.html") === -1) nav.innerHTML = html;
+}
 function splitStamp(){
+  splitNav();
   if (document.querySelector(".updated")) return true;
   var header = document.querySelector("header");
   if (!header) return false;
-  var text = "LAST UPDATED: Fri Sep 11, 2026 9:17 p.m. ET";
+  var text = "LAST UPDATED: Fri Sep 11, 2026 10:02 p.m. ET";
   try {
     var parts = new Intl.DateTimeFormat("en-US", {
       timeZone: "America/New_York",
@@ -26,6 +37,7 @@ function splitStamp(){
   return true;
 }
 (function(){
+  splitNav();
   if (splitStamp()) return;
   var n = 0;
   var t = setInterval(function(){

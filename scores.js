1b2c1f502a377d5f572d9b4f52442683fb7a017e
CJD0314 <danielsc0314@gmail.com>
2026-09-11 21:54:43 -0400

    Add live and final scores overlay

---
+++ scores.js
@@ -0,0 +1,19 @@
+window.SCORES = {
+  "NE at SEA": "FINAL SEA 13-10",
+  "SF at LAR": "FINAL SF 27-7",
+  "FAMU at MIA": "FINAL MIA 77-7",
+  "PIT at CHC": "FINAL CHC 12-2",
+  "COL at DET": "FINAL DET 6-2",
+  "LAA at WSH": "FINAL WSH 4-3",
+  "HOU at TB": "FINAL TB 3-1",
+  "VILL at LOU": "LIVE LOU 45-13 3Q",
+  "NORF at UVA": "LIVE UVA 49-3 4Q",
+  "MIZ at KU": "LIVE KU 14-7 2Q",
+  "NYM at NYY": "LIVE NYY 5-3 8th",
+  "BAL at TOR": "LIVE BAL 7-2 9th",
+  "KC at BOS": "LIVE KC 3-2 9th",
+  "LAD at MIA": "LIVE LAD 6-1 8th",
+  "PHI at ATL": "LIVE PHI 3-1 7th",
+  "CIN at MIL": "LIVE MIL 17-0 7th",
+  "CLE at MIN": "LIVE MIN 1-0 6th"
+};

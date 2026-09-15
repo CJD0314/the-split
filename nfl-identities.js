7a34701a4f2ca01673bfa98c18f49616205ddf76
CJD0314 <danielsc0314@gmail.com>
2026-09-14 20:42:53 -0400

    Clean identity layout: labeled rows, grouped sections

--- nfl-identities.js
+++ nfl-identities.js
@@ -5,10 +5,10 @@ function paintTeamIdentities(){
   const order = Object.keys(NFL_IDENTITIES).sort(function(a,b){
     return NFL_IDENTITIES[a].name.localeCompare(NFL_IDENTITIES[b].name);
   });
-  box.innerHTML = "<p class='note-head'>Week 1 tape with points and engines. Tap the club. Use it in the next full breakdown.</p>" +
+  box.innerHTML = "<p class='note-head'>Tap a club. Staff, tape, and the next-dive line live on the identity page.</p>" +
     order.map(function(k){
       const t = NFL_IDENTITIES[k];
-      return "<div class='g'><b><a class='full-link' href='nfl-team.html?t="+k+"'>"+t.name.toUpperCase()+"</a></b><span>"+t.style+" \u00b7 "+t.pf+"-"+t.pa+" \u00b7 "+t.engine+"</span></div>";
+      return "<div class='g'><b><a class='full-link' href='nfl-team.html?t="+k+"'>"+t.name.toUpperCase()+"</a></b><span>"+t.style+"</span><span>"+t.pf+"-"+t.pa+" · "+t.engine+"</span></div>";
     }).join("");
 }
 paintTeamIdentities();

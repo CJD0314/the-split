22842d542e180cf41c50e1d7c5eeb438f920fc36
CJD0314 <danielsc0314@gmail.com>
2026-09-14 21:58:02 -0400

    Team identity list is name and logo only

--- nfl-identities.js
+++ nfl-identities.js
@@ -5,10 +5,10 @@ function paintTeamIdentities(){
   const order = Object.keys(NFL_IDENTITIES).sort(function(a,b){
     return NFL_IDENTITIES[a].name.localeCompare(NFL_IDENTITIES[b].name);
   });
-  box.innerHTML = "<p class='note-head'>Tap a club. Staff, tape, and the next-dive line live on the identity page.</p>" +
-    order.map(function(k){
-      const t = NFL_IDENTITIES[k];
-      return "<div class='g'><b><a class='full-link' href='nfl-team.html?t="+k+"'>"+t.name.toUpperCase()+"</a></b><span>"+t.style+"</span><span>"+t.pf+"-"+t.pa+" · "+t.engine+"</span></div>";
-    }).join("");
+  box.innerHTML = order.map(function(k){
+    const t = NFL_IDENTITIES[k];
+    const logo = "https://a.espncdn.com/i/teamlogos/nfl/500/" + k + ".png";
+    return "<a class='g' href='nfl-team.html?t="+k+"'><img src='"+logo+"' alt=''><b>"+t.name.toUpperCase()+"</b></a>";
+  }).join("");
 }
 paintTeamIdentities();
